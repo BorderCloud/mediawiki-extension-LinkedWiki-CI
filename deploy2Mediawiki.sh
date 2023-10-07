b5504e2c69d579f6a6ca5c2bda13dc109b5b42d4
Karima Rafes <karima.rafes@gmail.com>
2023-10-07 08:44:26 +0200

    fix

--- deploy2Mediawiki.sh
+++ deploy2Mediawiki.sh
@@ -5,7 +5,7 @@
 echo "Allow to execute Lua"
 pwd
 cd $2/w
-sudo chmod a+x extensions/Scribunto/includes/Engines/LuaStandalone/binaries/lua5_1_5_linux_64_generic/lua  2>&1
-sudo chmod a+x extensions/Scribunto/includes/engines/LuaStandalone/binaries/lua5_1_5_linux_64_generic/lua  2>&1
+sudo chmod a+x extensions/Scribunto/includes/Engines/LuaStandalone/binaries/lua5_1_5_linux_64_generic/lua  2>/dev/null
+sudo chmod a+x extensions/Scribunto/includes/engines/LuaStandalone/binaries/lua5_1_5_linux_64_generic/lua  2>/dev/null
 
 
